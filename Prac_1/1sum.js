b810b9d3bbb70505402b2243c608d3c0fffa3cca
HIDIPIKA <kcdipika03112061@gmail.com>
2023-08-18 20:55:37 +0545

    Day1

--- Prac_1/1sum.js
+++ Prac_1/1sum.js
@@ -1,12 +1,14 @@
+const main = document.querySelector(".main")
 
 const btn1 = document.getElementById("clickme")
-const main = document.querySelector(".main")
-const text=document.getElementById("haha")
 const btn2=document.getElementById("nepal")
-const btn3=document.getElementById("china")
+
+const text=document.getElementById("haha")
 const n1=document.getElementById("num1")
 const n2=document.getElementById("num2")
 
+
+//to change the background color with each click on btn1
 i = 0;
 let colors = ['Red','Yellow','blue','purple','orange','gray']
 btn1.addEventListener("click",()=>{
@@ -14,32 +16,32 @@ btn1.addEventListener("click",()=>{
     main.style.backgroundColor = colors[i];
     i++;
     // console.log(i)
-    let listy = ["Dipika","Saurav","Krish","Mansa"]
-    for(i=0;i<listy.length;i++){
-        console.log(listy[i])
-    }
 })
+
+
+    //to display a list in a loop
+    // let listy = ["Dipika","Saurav","Krish","Mansa"]
+    // for(i=0;i<listy.length;i++){
+    //     console.log(listy[i])
+    // }
+
 
 //ask user to enter two num // show the sum of result either in console or webpage
 // function sum(a,b){
 //     c=eval(a+b)
-//     console.log(c) 
-    
+//     console.log(c)  
 // }
 // sum(a,b)
 
 
+//to display sum
 btn2.addEventListener("click",()=>{
     let v=eval(n1.value)+eval(n2.value);
     text.textContent=v;
 })
 
-btn3.addEventListener("click",()=>{
-   let c=eval(n1.value)-eval(n2.value);
-   text.textcontent=c;
-   console.log("clicked")
-})
 
+//to display time in seconds
 // setInterval(()=>{
 //     krish.textContent= Date()
 // },1000)
